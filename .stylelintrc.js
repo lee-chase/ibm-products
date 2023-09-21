c1391fd663bcf82f9b42ceb82b4f0caee3ea62c9
Lee Chase <lee.chase@uk.ibm.com>
2023-09-21 18:12:39 +0100

    chore: with react-dnd

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   root: true,
   extends: ['stylelint-config-ibm-products'],
+  overrides: [
+    {
+      files: ['**/*.scss'],
+      customSyntax: 'postcss-scss',
+    },
+  ],
   rules: {
     'import-notation': 'string',
   },
